82e589df3298106d6eb0463b12eec8efb0ec8fe0
Lenny Martinez <lennymartinezd@gmail.com>
2018-05-10 22:54:35 -0400

    redo 2010, 2011 csv files; clean them as well

--- FBI_csv_table13_table14/cleanCSV.py
+++ FBI_csv_table13_table14/cleanCSV.py
@@ -2,7 +2,7 @@ import csv
 
 file = []
 cleanFile = []
-with open('1_annualCSV/hateCrimeStatistics_2016.csv') as csvDataFile:
+with open('1_annualCSV/hateCrimeStatistics_2010.csv') as csvDataFile:
 	csvReader = csv.reader(csvDataFile)
 	for row in csvReader:
 		file.append(row)
@@ -21,8 +21,8 @@ for i in range(0,len(file)):
 	# population = file[i][8].strip()
 
 
-	if (year == "") or (year == "2016"):
-		cleanFile[i][0] = "2016"
+	if (year == "") or (year == "2010"):
+		cleanFile[i][0] = "2010"
 
 	if state == "Nan":
 		cleanFile[i][1] = file[i-1][1]
@@ -34,6 +34,6 @@ for i in range(0,len(file)):
 		cleanFile[i][3] = "@@DELETE ME@@"
 
 
-with open("fbi_2016.csv","w+") as my_csv:
+with open("fbi_2011.csv","w+") as my_csv:
 			csvWriter = csv.writer(my_csv,delimiter=',')
 			csvWriter.writerows(cleanFile)
