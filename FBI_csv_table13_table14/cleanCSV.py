aa5658f9dc37a461aa0fb0fd9c70ee352df52073
Lenny Martinez <lennymartinezd@gmail.com>
2018-05-12 16:42:37 -0400

    give up trying to create a simple table. not sure how backfill and forward fill data with csv and python.

--- FBI_csv_table13_table14/cleanCSV.py
+++ FBI_csv_table13_table14/cleanCSV.py
@@ -2,44 +2,48 @@ import csv
 
 file = []
 cleanFile = []
-with open('4_annualTestAlt/fbiMain_one.csv') as csvDataFile:
+with open('4_annualTestAlt/fbiMain_simple.csv') as csvDataFile:
 	csvReader = csv.reader(csvDataFile)
 	for row in csvReader:
 		file.append(row)
 
 cleanFile = file
+#int j = 38
+for j in range(4,38):
+	for i in range(1,len(file)):
+		if (file[i][j].strip() == ""):
+			cleanFile[i][j] = file[i-1][j].strip()
 
-for i in range(0,len(file)):
-	pop10 = file[i][8].strip()
-	pop11 = file[i][13].strip()
-	pop12 = file[i][18].strip()
-	pop13 = file[i][23].strip()
-	pop14 = file[i][28].strip()
-	pop15	= file[i][33].strip()
-	pop16 = file[i][38].strip()
+	# pop10 = file[i][8].strip()
+	# pop11 = file[i][13].strip()
+	# pop12 = file[i][18].strip()
+	# pop13 = file[i][23].strip()
+	# pop14 = file[i][28].strip()
+	# pop15	= file[i][33].strip()
+	# pop16 = file[i][38].strip()
 	
-	if (pop10 == "") or (pop10 == "---") or (pop10 == "Nan"):
-		cleanFile[i][8] = "Nan"
+	# if (pop10 == "") or (pop10 == "---") or (pop10 == "Nan"):
+	# 	cleanFile[i][8] = "Nan"
 	
-	if (pop11 == "") or (pop11 == "---") or (pop11 == "Nan"):
-		cleanFile[i][13] = "Nan"
+	# if (pop11 == "") or (pop11 == "---") or (pop11 == "Nan"):
+	# 	cleanFile[i][13] = "Nan"
 
-	if (pop12 == "") or (pop12 == "---") or (pop12 == "Nan"):
-		cleanFile[i][18] = "Nan"
+	# if (pop12 == "") or (pop12 == "---") or (pop12 == "Nan"):
+	# 	cleanFile[i][18] = "Nan"
 
-	if (pop13 == "") or (pop13 == "---") or (pop13 == "Nan"):
-		cleanFile[i][23] = "Nan"
+	# if (pop13 == "") or (pop13 == "---") or (pop13 == "Nan"):
+	# 	cleanFile[i][23] = "Nan"
 
-	if (pop14 == "") or (pop14 == "---") or (pop14 == "Nan"):
-		cleanFile[i][28] = "Nan"
+	# if (pop14 == "") or (pop14 == "---") or (pop14 == "Nan"):
+	# 	cleanFile[i][28] = "Nan"
 
-	if (pop15 == "") or (pop15 == "---") or (pop15 == "Nan"):
-		cleanFile[i][33] = "Nan"
+	# if (pop15 == "") or (pop15 == "---") or (pop15 == "Nan"):
+	# 	cleanFile[i][33] = "Nan"
 
-	if (pop16 == "") or (pop16 == "---") or (pop16 == "Nan"):
-		cleanFile[i][38] = "Nan"	
+	# if (pop16 == "") or (pop16 == "---") or (pop16 == "Nan"):
+	# 	cleanFile[i][38] = "Nan"	
 
 
-with open("fbiMain_oneClean.csv","w+") as my_csv:
+with open("fbiMain_simpleClean.csv","w+") as my_csv:
 			csvWriter = csv.writer(my_csv,delimiter=',')
 			csvWriter.writerows(cleanFile)
