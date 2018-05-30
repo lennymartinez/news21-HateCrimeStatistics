8ade7a704198184f3439da779b08758e467ccc0b
Lenny Martinez <lennymartinezd@gmail.com>
2018-05-29 22:45:46 -0700

    start statistical analysis of population, number of quarters with 0 reports, number of quarters with count dnr

--- FBI_csv_table13_table14/histograms.py
+++ FBI_csv_table13_table14/histograms.py
@@ -4,11 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-with open('fbiMain_population.csv', newline='', encoding='utf-8') as f:
+with open('fbiMain_v2.csv', newline='', encoding='utf-8') as f:
 	reader = csv.reader(f)
 	rows = [row for row in reader]
-print("we have "+str(len(rows))+" rows")
+print("we have "+str(len(rows)-1)+" rows")
 
 population = []
+dnr = []
+zero = []
 for i in range(1,len(rows)):
-	population.append(rows[i][2])
+	population.append(int(rows[i][2]))
+	dnr.append(int(rows[i][3]))
+	zero.append(int(rows[i][4]))
+
+print("Population")
+print("average: ", np.average(population))
+print("variance: ", np.var(population))
+print("---")
+print("DNR quarters")
+print("average: ", np.average(dnr))
+print("variance: ", np.var(dnr))
+print("---")
+print("Zero quarters")
+print("average: ", np.average(zero))
+print("variance: ", np.var(zero))
+
+hist, binEdges = np.histogram(population, bins=[0,100,1000,10000,100000,1000000])
+print(hist)
+print(binEdges)
